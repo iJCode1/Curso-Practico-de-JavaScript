e9584f685ff09bb4f4e4e62b558e7013e41a6393
Joel <joeldome17@gmail.com>
2021-06-26 10:03:20 -0500

    Added Isosceles Triangle

--- js/figuras.js
+++ js/figuras.js
@@ -80,3 +80,30 @@ function circulo2(){
 
     alert(`El perímetro del circulo es: ${perimetroCi}cm y el área es: ${areaCi}cm^2`);
 }
+
+//Triangulo Isósceles
+function trianguloIso(){
+    let lado1 = document.getElementById("isoLado1");
+    let iLado1 = parseFloat(lado1.value);
+
+    let lado2 = document.getElementById("isoLado2");
+    let iLado2 = parseFloat(lado2.value);
+
+    let base = document.getElementById("isoBase");
+    let iBase = parseFloat(base.value);
+
+    if(iLado1 != iLado2){
+        alert("No se puede calcular debido a que los lados son diferentes");
+    }else{
+        const pequenoLado2 = iBase /2; 
+        const pequenoBase = iLado1;
+
+        const pequenoLado2Cuadrado = pequenoLado2 * pequenoLado2;
+        const pequenoBaseCuadrado = pequenoBase * pequenoBase;
+         
+        let pequenoLado1 = Math.sqrt(pequenoBaseCuadrado - pequenoLado2Cuadrado);
+
+        const grandeAltura = pequenoLado1;
+        alert(`La altura del triangulo es: ${grandeAltura}cm`);
+    }
+}
