0917218e3c0c0ac1404db6dc0d16012b88b9290a
Joel <joeldome17@gmail.com>
2021-06-26 02:17:22 -0500

    User data request from html

--- js/figuras.js
+++ js/figuras.js
@@ -8,6 +8,15 @@ function cuadrado(ladoCuadrado){
     console.groupEnd();
 }
 
+function cuadrado2(){
+    let elemento = document.getElementById("cLado");
+    let ladoCuadrado = parseFloat(elemento.value);
+    let perimetroC = ladoCuadrado + ladoCuadrado + ladoCuadrado + ladoCuadrado;
+    let areaC = ladoCuadrado * ladoCuadrado;
+
+    alert(`El perímetro del cuadrado es: ${perimetroC}cm y el área es: ${areaC}cm^2`);
+}
+
 cuadrado(156);
 
 
@@ -20,6 +29,26 @@ function trinagulo(lado1, lado2, base, altura){
     console.log(`El perímetro del triangulo es: ${perimetroT}cm`)
     console.log(`El área del triangulo es: ${areaT}cm^2`)
     console.groupEnd();
+}
+
+function triangulo2(){
+    let lado1 = document.getElementById("tLado1");
+    let tLado1 = parseFloat(lado1.value);
+
+    let lado2 = document.getElementById("tLado2");
+    let tLado2 = parseFloat(lado2.value);
+
+    let base = document.getElementById("tBase");
+    let tBase = parseFloat(base.value);
+
+    let altura = document.getElementById("tAltura");
+    let tAltura = parseFloat(altura.value);
+    
+    let tPerimetro = tLado1 + tLado2 + tBase;
+    let tArea = (tBase*tAltura)/2;
+
+    alert(`El perímetro del triangulo es: ${tPerimetro}cm y el área es de ${tArea}cm^2`);
+
 }
 
 trinagulo(6, 6, 4, 5.5);
@@ -39,3 +68,15 @@ function circulo(radio){
 }
 
 circulo(4);
+
+function circulo2(){
+    let radio = document.getElementById("cRadio");
+    let cRadio = parseFloat(radio.value);
+    const PI = Math.PI;
+
+    let diametroCi = 2 * cRadio;
+    let perimetroCi = diametroCi * PI;
+    let areaCi = PI * (cRadio**2);
+
+    alert(`El perímetro del circulo es: ${perimetroCi}cm y el área es: ${areaCi}cm^2`);
+}
