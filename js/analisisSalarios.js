703820a0f140db8dcf9e9ce44a0ca1fcb11c0ea9
Joel <joeldome17@gmail.com>
2021-07-05 03:20:09 -0500

    Workshop 4, Adding salaries and calculating the average

---
+++ js/analisisSalarios.js
@@ -0,0 +1,51 @@
+/*
+    Analizando los salarios de México
+*/
+
+//Obteniendo los salarios unicamente.
+const obtenerSalarios = arreglo =>{
+    return arreglo.map( ele =>{
+        return ele.salary;
+    })
+};
+//console.log(obtenerSalarios(salariosMexico));
+// (24) [37000, 28434, 34274, 18364, 18700, 12400, 16560, 18373, 19373, 17823, 19734, 27473, 19372, 10834, 16723, 18947, 36427, 19374, 36821, 36482, 26483, 18492, 46283, 25372]
+
+//Ordenando los salarios
+const ordenarSalarios = obtenerSalarios(salariosMexico).sort((elementoA, elementoB) =>{
+    return elementoA - elementoB;
+});
+// console.log(ordenarSalarios);
+// (24) [10834, 12400, 16560, 16723, 17823, 18364, 18373, 18492, 18700, 18947, 19372, 19373, 19374, 19734, 25372, 26483, 27473, 28434, 34274, 36427, 36482, 36821, 37000, 46283]
+
+/* 
+    Calculando la Mediana 
+*/
+const esPar = arreglo =>{
+    if(arreglo.length % 2 === 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+const obtenerMedio = array => {
+    return parseInt((array.length) / 2);
+}
+
+let $mediana = 0;
+
+const mediana = array => {
+    if(esPar(array)){
+        let valorActual = array[obtenerMedio(array)];
+        let valorAnterior = array[obtenerMedio(array) - 1];
+        // console.log(`Valor actual: ${valorActual}, valorAnterior: ${valorAnterior}`);
+        $mediana = ( valorActual + valorAnterior ) / 2;
+    }else{
+        $mediana = array[obtenerMedio(array)];
+    }
+    return $mediana;
+}
+
+console.log(`La mediana es: ${mediana(ordenarSalarios)}`);
+//La mediana es: 19373.5
---
+++ js/salarios.js
@@ -0,0 +1,102 @@
+/*
+    Agregando los salarios de México
+*/
+
+const salariosMexico = [];
+
+salariosMexico.push({
+    name: "Joel",
+    salary: 37000
+});
+salariosMexico.push({
+    name: "Julieta",
+    salary: 28434
+});
+salariosMexico.push({
+    name: "Keyla",
+    salary: 34274
+});
+salariosMexico.push({
+    name: "Samuel",
+    salary: 18364
+});
+salariosMexico.push({
+    name: "Elias",
+    salary: 18700
+});
+salariosMexico.push({
+    name: "Jose",
+    salary: 12400
+});
+salariosMexico.push({
+    name: "Andrea",
+    salary: 16560
+});
+salariosMexico.push({
+    name: "Carmen",
+    salary: 18373
+});
+salariosMexico.push({
+    name: "Carlos",
+    salary: 19373
+});
+salariosMexico.push({
+    name: "Agustin",
+    salary: 17823
+});
+salariosMexico.push({
+    name: "Enrique",
+    salary: 19734
+});
+salariosMexico.push({
+    name: "Lizbeth",
+    salary: 27473
+});
+salariosMexico.push({
+    name: "Cesar",
+    salary: 19372
+});
+salariosMexico.push({
+    name: "Oscar",
+    salary: 10834
+});
+salariosMexico.push({
+    name: "Antonio",
+    salary: 16723
+});
+salariosMexico.push({
+    name: "Camila",
+    salary: 18947
+});
+salariosMexico.push({
+    name: "Carla",
+    salary: 36427
+});
+salariosMexico.push({
+    name: "Esmeralda",
+    salary: 19374
+});
+salariosMexico.push({
+    name: "Pepe",
+    salary: 36821
+});
+salariosMexico.push({
+    name: "Monica",
+    salary: 36482
+});
+salariosMexico.push({
+    name: "Erick",
+    salary: 26483
+});
+salariosMexico.push({
+    name: "Rosa",
+    salary: 18492
+});
+salariosMexico.push({
+    name: "Marisela",
+    salary: 46283
+});
+salariosMexico.push({
+    name: "Martha",
+    salary: 25372
+});
